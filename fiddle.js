2dc3290cfcf6e874b37c76f052a3136b175556e5
Nzar Tofiq <nt@holograph.digital>
2016-10-04 17:26:54 +0100

    all three versions

--- fiddle.js
+++ fiddle.js
@@ -1,38 +1,47 @@
-var userName, userLink, userInfo, data,
-	match, str = '',
-	entry, $results, results, template;
-userName = 'binaryben';
-userLink = '//jsfiddle.net/user/' + userName + '/fiddles/';
-userInfo = '<a href="' + userLink + '">' + userName + '</a>'
-$('.user_info').html(userInfo);
-$.ajax({
-	url: '//fiddle.jshell.net/api/user/' + userName + '/demo/list.json?&start=0&sort=date&start=0&limit=99999&order=desc'
-}).done(function (allfiddles) {
-	var json = jQuery.parseJSON(allfiddles);
-	//console.log(json);
-});
-
-function search(str, data) {
-	$results = $(data).map(function (entry) {
-		match = data[entry].title.toLowerCase().indexOf(str.toLowerCase()) > -1;
-		return match ? entry : null;
+(function getUserName() {
+	var userName, userLink, userInfo, data,
+		match, str = '',
+		entry, results;
+	$('.find_user_btn').click(function (e) {
+		userName = $('.find_user_text').val();
+		userLink = '//jsfiddle.net/user/' + userName + '/fiddles/';
+		userInfo = '<a href="' + userLink + '">' + userName + '</a>';
+		$('.user_info').html(userInfo);
+		$(this).disable();
+		getFiddles(userName)
 	});
-	return $results;
-}
+}());
 
-function update(str, data) {
-	if (str === '') {
-		str = ' ';
-	}
-	results = search(str, data);
-	$('.fiddles').html("");
-	$(results).each(function (i, v) {
-		template = '<div class="title"><a href="http://' + data[v].url + '">' + data[v].title + '</a></div>';
-		template += '<div class = "description" > ' + data[v].description + '</div>';
-		$('.fiddles').append(template);
+function getFiddles(userName) {
+	$.ajax({
+		url: '//fiddle.jshell.net/api/user/' + userName + '/demo/list.json?&start=0&sort=date&start=0&limit=99999&order=desc'
+	}).done(function (allfiddles) {
+		var json = jQuery.parseJSON(allfiddles);
+		//console.log(json);
+		function search(str, data) {
+			result = $(data).map(function (entry) {
+				match = String(data[entry]).toLowerCase().indexOf(str.toLowerCase()) > -1;
+				return match ? data[entry] : null;
+			});
+			return results;
+		}
+
+		function update(str, data) {
+			if (str === '') {
+				str = ' ';
+			}
+			results = search(str, data);
+
+			function render() {
+				var source = $("#fiddles").html();
+				var template = Handlebars.compile(source);
+				$('.fiddles').html(template(results));
+			}
+			render();
+		}
+		$('.search_fiddles_text').keyup(function () {
+			update($(this).val());
+		});
+		update(' ', data);
 	});
 }
-$('.search_fiddles_text').keyup(function () {
-	update($(this).val());
-});
-update(' ', data);
---
+++ task_1/fiddle.js
@@ -0,0 +1,37 @@
+var userName, userLink, userInfo, data,
+	match, str = '',
+	entry, results, template;
+userName = 'binaryben';
+userLink = '//jsfiddle.net/user/' + userName + '/fiddles/';
+userInfo = '<a href="' + userLink + '">' + userName + '</a>'
+$('.user_info').html(userInfo);
+$.ajax({
+	url: '//fiddle.jshell.net/api/user/' + userName + '/demo/list.json?&start=0&sort=date&start=0&limit=99999&order=desc'
+}).done(function (allfiddles) {
+	var json = jQuery.parseJSON(allfiddles);
+	//console.log(json);
+	function search(str, data) {
+		results = $(data).map(function (entry) {
+			match = String(data[entry]).toLowerCase().indexOf(str.toLowerCase()) > -1;
+			return match ? data[entry] : null;
+		});
+		return results;
+	}
+
+	function update(str, data) {
+		if (str === '') {
+			str = ' ';
+		}
+		results = search(str, data);
+		$('.fiddles').html("");
+		$(results).each(function (i, v) {
+			template = '<div class="title"><a href="http://' + data[v].url + '">' + data[v].title + '</a></div>';
+			template += '<div class = "description" > ' + data[v].description + '</div>';
+			$('.fiddles').append(template);
+		});
+	}
+	$('.search_fiddles_text').keyup(function () {
+		update($(this).val());
+	});
+	update(' ', data);
+});
